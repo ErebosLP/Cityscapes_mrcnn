7cb1f03b9552d818584e0758eb5156c323e9ec88
ErebosLP <106330621+ErebosLP@users.noreply.github.com>
2022-05-28 15:43:03 +0200

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -175,7 +175,7 @@ def main():
 
     
 
-    threshold_pred = 0.05 # ab welchem threshold sollen predictions angezeigt werden
+    threshold_pred = 0.1 # ab welchem threshold sollen predictions angezeigt werden
     resize_factor = [1]#,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.275,0.25,0.225,0.2,0.175,0.15,0.125,0.1,0.075,0.05,0.025]
     
     # ==================================
